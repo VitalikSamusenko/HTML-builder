498974485e01ca607deacbea1c4624d1b4d91f4a
VitalikSamusenko <vitaliksamusenko1997@gmail.com>
2022-05-21 17:18:32 +0300

    feat: complete 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+let data = '';
+
+readStream.on('data', chunk => data += chunk);
+readStream.on('end', () => console.log(data));
+readStream.on('error', err => console.log('Error: ', err.message));
